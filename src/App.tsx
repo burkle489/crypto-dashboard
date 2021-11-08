e5ccc062519db83095cfd8d37f556ea7f9c575ff
Tayler Burke <taylerburke48@gmail.com>
2021-11-08 18:38:53 +0000

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import { CoinbaseTracker } from './components/CoinbaseTracker/CoinbaseTracker';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
 function App() {
   return (
     <div className="App">
       <CoinbaseTracker />
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
