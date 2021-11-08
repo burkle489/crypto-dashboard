93b45f6a63b0736c21e5323ed1844812c470ffd3
Tayler Burke <taylerburke48@gmail.com>
2021-11-08 18:58:00 +0000

    added graphql endpoint

---
+++ src/graphql/queries.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPortfolioAsset = /* GraphQL */ `
+  query GetPortfolioAsset($id: ID!) {
+    getPortfolioAsset(id: $id) {
+      id
+      assetPair
+      costPerCoin
+      totalCoins
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listPortfolioAssets = /* GraphQL */ `
+  query ListPortfolioAssets(
+    $filter: ModelPortfolioAssetFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPortfolioAssets(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        assetPair
+        costPerCoin
+        totalCoins
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePortfolioAsset = /* GraphQL */ `
+  subscription OnCreatePortfolioAsset {
+    onCreatePortfolioAsset {
+      id
+      assetPair
+      costPerCoin
+      totalCoins
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdatePortfolioAsset = /* GraphQL */ `
+  subscription OnUpdatePortfolioAsset {
+    onUpdatePortfolioAsset {
+      id
+      assetPair
+      costPerCoin
+      totalCoins
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeletePortfolioAsset = /* GraphQL */ `
+  subscription OnDeletePortfolioAsset {
+    onDeletePortfolioAsset {
+      id
+      assetPair
+      costPerCoin
+      totalCoins
+      createdAt
+      updatedAt
+    }
+  }
+`;
